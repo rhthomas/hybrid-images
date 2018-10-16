cf60a37ebbeb45b1b6332962976521f6b175216c
Rhys Thomas <rhys97@me.com>
2018-10-16 13:47:41 +0100

    Now able to load image to opencv
    However, convolution is just displaying white. Need to also import the 
    image in full colour and perform the convolution on all channels.

--- hybrid.py
+++ hybrid.py
@@ -19,29 +19,33 @@ def define_args():
                     help="Path to output image file.")
     ap.add_argument("-v", "--visual", required=False,
                     help="Path to output visualisation file.")
-
+    # Return arguments.
     return vars(ap.parse_args())
 
 
-def convolution(image, kernel):
+def convolution(img, kernel):
     """ Run convolution.
 
-    This function executes the convolution between `image` and `kernel`.
+    This function executes the convolution between `img` and `kernel`.
     Note: `None` indicates that there is content yet to complete.
     """
+    image = cv2.imread(img, 0)
+    cv2.imshow('Input image', image)
     # Get size of image and kernel.
-
+    (image_h, image_w) = image.shape[:2]
+    (kernel_h, kernel_w) = kernel.shape[:2]
+    (pad_h, pad_w) = (kernel_h // 2, kernel_w // 2)
     # Create image to write to.
-    output = None
+    output = np.zeros(image.shape)
     # Slide kernel across every pixel.
-    for y in None:
-        for x in None:
+    for y in range(pad_h, image_h-pad_h):
+        for x in range(pad_w, image_w-pad_w):
             # Get center pixel.
-            center = None
+            center = image[y - pad_h:y + pad_h + 1, x - pad_w:x + pad_w + 1]
             # Perform convolution.
             conv = (center * kernel).sum()
             # Write back value to output image.
-            output[None] = k
+            output[y][x] = conv
 
     # Return the result of the convolution.
     return output
@@ -63,20 +67,21 @@ def low_pass(image):
 
     Returns low pass version of the input `image`.
     """
-
+    # Perform convolution with bluring kernel.
+    img = convolution(image, blur)
     # Return result.
     return img
 
 
-def low_pass(image):
+def high_pass(image):
     """ Perform HPF.
 
     Returns low pass version of the input `image`.
     """
     # Compute low pass of image.
-
+    img = low_pass(image)
     # Subtract from starting image.
-
+    img = image - img
     # Return result.
     return img
 
@@ -99,8 +104,18 @@ def main():
     with the kernel size argument, the convolution is performed and the
     resulting image is displayed.
     """
+    # Get arguments.
     args = define_args()
     print(args)
+    # Split kernel size.
+    (kW, kH) = args["kernel"].split('x')
+    # Run convolution.
+    images = args["image"]
+    smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+    cv2.imshow('Result of convolution',
+               convolution(images[0], smallBlur))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
 # Call the main function.
