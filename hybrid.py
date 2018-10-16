657783447404fddcbab6a5e6b5fdc6a2726c6d9d
Rhys Thomas <rhys97@me.com>
2018-10-16 12:35:06 +0100

    Initial commit.

---
+++ hybrid.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+import cv2
+import numpy as np
+
+
+""" convolution()
+This function executes the convolution between `image` and `kernel`.
+"""
+def convolution(image, kernel):
+
+    # Return the result of the convolution.
+    return result
+
+
+""" construct_kernels()
+Builds a dictionary of kernels based on the dimentions passed when the program
+is run.
+"""
+def construct_kernels():
+
+    # Return kernel dictionary.
+    return kernels
+
+
+""" low_pass()
+Returns low pass version of the input `image`.
+"""
+def low_pass(image):
+
+    # Return result.
+    return img
+
+
+""" high_pass()
+Returns high pass version of the input `image`.
+"""
+def low_pass(image):
+    # Compute low pass of image.
+
+    # Subtract from starting image.
+
+    # Return result.
+    return img
+
+
+""" output_vis()
+Display hybrid image comparison for report. Visualisation shows 5 images
+reducing in size to simulate viewing the image from a distance.
+"""
+def output_vis(image):
+
+    # Return the output visualisation.
+    return output
+
+
+""" main()
+This is the main function. Here the image is loaded into an array, along with
+the kernel size argument, the convolution is performed and the resulting image
+is displayed.
+"""
+def main():
+
+
+
+# Call the main function.
+if __name__ == "__main__":
+    main()
