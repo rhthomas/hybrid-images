29282512570f4c77243c042a5d87b4586a583d98
Rhys Thomas <rhys97@me.com>
2019-06-11 00:02:59 +0100

    Formatted using black.

--- hybrid.py
+++ hybrid.py
@@ -11,21 +11,42 @@ def define_args():
     """ Defines the arguments required to run the program.
     """
     ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", nargs=2, required=True,
-                    help="Path to input images.")
-    ap.add_argument("-k", "--kernel", nargs=2, type=int,
-                    help="Kernal size, e.g. 5 7. Note: first image in list will\
-                    be used.")
-    ap.add_argument("-c", "--cutoff", nargs=2, type=int,
-                    help="Gaussian cutoff frequencies, e.g. 5 5.")
-    ap.add_argument("-o", "--output", required=False,
-                    help="Path to output image file.")
-    ap.add_argument("-v", "--visual", required=False,
-                    help="Path to output visualisation file.")
-    ap.add_argument("-f", "--fourier", default=False, action='store_true',
-                    help="Use Fourier convolution.")
-    ap.add_argument("-s", "--sobel", default=False, action='store_true',
-                    help="Run Sobel edge detection on the first image.")
+    ap.add_argument(
+        "-i", "--image", nargs=2, required=True, help="Path to input images."
+    )
+    ap.add_argument(
+        "-k",
+        "--kernel",
+        nargs=2,
+        type=int,
+        help="Kernal size, e.g. 5 7. Note: first image in list will\
+                    be used.",
+    )
+    ap.add_argument(
+        "-c",
+        "--cutoff",
+        nargs=2,
+        type=int,
+        help="Gaussian cutoff frequencies, e.g. 5 5.",
+    )
+    ap.add_argument("-o", "--output", required=False, help="Path to output image file.")
+    ap.add_argument(
+        "-v", "--visual", required=False, help="Path to output visualisation file."
+    )
+    ap.add_argument(
+        "-f",
+        "--fourier",
+        default=False,
+        action="store_true",
+        help="Use Fourier convolution.",
+    )
+    ap.add_argument(
+        "-s",
+        "--sobel",
+        default=False,
+        action="store_true",
+        help="Run Sobel edge detection on the first image.",
+    )
 
     # Return arguments.
     return vars(ap.parse_args())
@@ -46,14 +67,14 @@ def convolution(img, kernel):
     # Create image to write to.
     output = np.zeros(image.shape)
     # Slide kernel across every pixel.
-    for y in range(pad_h, image_h-pad_h):
-        for x in range(pad_w, image_w-pad_w):
+    for y in range(pad_h, image_h - pad_h):
+        for x in range(pad_w, image_w - pad_w):
             # If coloured, loop for colours.
             for colour in range(image.shape[2]):
                 # Get center pixel.
-                center = image[y - pad_h:y + pad_h + 1,
-                               x - pad_w:x + pad_w + 1,
-                               colour]
+                center = image[
+                    y - pad_h : y + pad_h + 1, x - pad_w : x + pad_w + 1, colour
+                ]
                 # Perform convolution and map value to [0, 255].
                 # Write back value to output image.
                 output[y, x, colour] = (center * kernel).sum() / 255
@@ -74,8 +95,7 @@ def fourier(img, kernel):
     padded_kernel = np.zeros(image.shape[:2])
     start_h = (image_h - kernel_h) // 2
     start_w = (image_w - kernel_w) // 2
-    padded_kernel[start_h:start_h + kernel_h,
-                  start_w:start_w + kernel_w] = kernel
+    padded_kernel[start_h : start_h + kernel_h, start_w : start_w + kernel_w] = kernel
     # Create image to write to.
     output = np.zeros(image.shape)
     # Run FFT on all 3 channels.
@@ -125,7 +145,7 @@ def high_pass(image, cutoff):
     """ Generate high pass filter of image. This is simply the image minus its
     low passed result.
     """
-    return (cv2.imread(image)/255) - low_pass(image, cutoff)
+    return (cv2.imread(image) / 255) - low_pass(image, cutoff)
 
 
 def hybrid_image(image, cutoff):
@@ -135,11 +155,11 @@ def hybrid_image(image, cutoff):
     # Perform low pass filter and export.
     print("[" + image[0] + "]\tGenerating low pass image...")
     low = low_pass(image[0], cutoff[0])
-    cv2.imwrite('low.jpg', low * 255)
+    cv2.imwrite("low.jpg", low * 255)
     # Perform high pass filter and export.
     print("[" + image[1] + "]\tGenerating high pass image...")
     high = high_pass(image[1], cutoff[1])
-    cv2.imwrite('high.jpg', (high + 0.5) * 255)
+    cv2.imwrite("high.jpg", (high + 0.5) * 255)
     # Return hybrid image.
     print("Creating hybrid image...")
     return low + high
@@ -169,8 +189,9 @@ def output_vis(image):
     # Add images to stack.
     current_x = 0
     for img in image_list:
-        stack[max_height-img.shape[0]:,
-              current_x:img.shape[1] + current_x, :] = img
+        stack[
+            max_height - img.shape[0] :, current_x : img.shape[1] + current_x, :
+        ] = img
         current_x += img.shape[1] + gap
 
     # Return the result.
@@ -202,9 +223,9 @@ def main():
             # No need to crop the fourier image.
             hybrid = hybrid_image(images, cutoff)
         else:
-            hybrid = hybrid_image(images,
-                                  cutoff)[4 * max(cutoff):-4 * max(cutoff),
-                                          4 * max(cutoff):-4 * max(cutoff)]
+            hybrid = hybrid_image(images, cutoff)[
+                4 * max(cutoff) : -4 * max(cutoff), 4 * max(cutoff) : -4 * max(cutoff)
+            ]
         # Save resulting images.
         cv2.imwrite(args["output"], hybrid * 255)
         if args["visual"] is not None:
@@ -212,12 +233,12 @@ def main():
             cv2.imwrite(args["visual"], output_vis(hybrid) * 255)
     elif args["sobel"]:
         # Run Sobel edge detection.
-        sobel_x = fourier(images[0], 255 * np.array([[1, 0, -1],
-                                                     [2, 0, -2],
-                                                     [1, 0, -1]]))
-        sobel_y = fourier(images[0], 255 * np.array([[1, 2, 1],
-                                                     [0, 0, 0],
-                                                     [-1, -2, -1]]))
+        sobel_x = fourier(
+            images[0], 255 * np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+        )
+        sobel_y = fourier(
+            images[0], 255 * np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+        )
         # Save resulting images.
         cv2.imwrite("sobel_x.jpg", sobel_x)
         cv2.imwrite("sobel_y.jpg", sobel_y)
